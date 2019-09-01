adda24e93fcf23b7566ee3ef3a6ae743135d03b8
Jon <jon_uk39@hotmail.com>
2019-09-01 13:37:32 +0100

    Misc refactoring

--- RaspberryPi/Setup/aptget_all.sh
+++ RaspberryPi/Setup/aptget_all.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install tightvncserver avahi-daemon hostapd bridge-utils -y
+#sudo apt-get install tightvncserver avahi-daemon hostapd bridge-utils -y
+#Don't think we need to get avahi-daemon on latest version of Raspian
+sudo apt-get install avahi-daemon hostapd bridge-utils -y
